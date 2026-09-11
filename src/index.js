94f9c659d6999d888537b11d87595cb7e371de51
ttran5 <301823648+Impykins0@users.noreply.github.com>
2026-09-10 19:36:44 -0600

    feat: Example Express server skeleton

--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { env } from "cloudflare:workers";
+import { httpServerHandler } from "cloudflare:node";
+import express from "express";
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
-};
+const app = express();
+
+// Error messages
+const SERVER_ERROR_MSG = 'Internal server error.';
+const NOT_FOUND_ERROR_MSG = '404: not found.';
+
+// Add endpoint middleware
+app.use(express.json());
+
+// Test to see if the server works
+app.get("/test", (req, res) => {
+	res.json({ 
+        message: "The server works!" 
+    });
+});
+
+// Request does not contain a valid endpoint
+app.use((req, res, next) => {
+    raiseError(next, 404, NOT_FOUND_ERROR_MSG);
+});
+
+app.listen(3000);
+export default httpServerHandler({ port: 3000 });
+
+/**
+ * Forwards an HTTP error to the error-handling middleware.
+ * @param {express.NextFunction} next - The "next" middleware function
+ * @param {number} status - The HTTP status code to return
+ * @param {string} message - The error message to send
+ * @returns {void}
+ */
+function raiseError(next, status, message) {
+    const err = new Error(message);
+    err.status = status;
+    next(err);
+}
+
+/**
+ * Middleware that handles different types of errors. Functions that call "next"
+ * with an error object are handled by this middleware appropriately.
+ * @param {Error} err - Error that was encountered
+ * @param {express.Request} req - The request object
+ * @param {express.Response} res - The response object to send error message to
+ * @param {express.NextFunction} next - The "next" middleware function
+ * @returns {void} 
+ */
+function errorHandler(err, req, res, next) {
+    res.status(err.status).json({
+        error: err.message,
+    });
+}
+
+// Adds error handling to the middleware stack
+app.use(errorHandler);
--- test/index.spec.js
+++ test/index.spec.js
@@ -7,19 +7,24 @@ import {
 import { describe, it, expect } from "vitest";
 import worker from "../src";
 
-describe("Hello World worker", () => {
-	it("responds with Hello World! (unit style)", async () => {
-		const request = new Request("http://example.com");
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+describe("Balaladle server", () => {
+	it("responds to /test with a server working message", async () => {
+        const response = await SELF.fetch("http://example.com/test");
+
+        expect(response.status).toBe(200);
+        expect(await response.json()).toEqual({
+            message: "The server works!",
+        });
 	});
 
-	it("responds with Hello World! (integration style)", async () => {
-		const response = await SELF.fetch("http://example.com");
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+    it("returns HTTP status 404 for an invalid endpoint", async () => {
+        const response = await SELF.fetch("http://example.com/impykins");
+
+        const data = await response.json();
+
+        expect(response.status).toBe(404);
+        expect(data).toEqual({
+            error: "404: not found.",
+        });
+    });
 });
